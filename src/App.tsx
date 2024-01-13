3a77fc7f4df9a1b5702805599c23b18b9c28d66a
mahdi gh <mahdiGhaffari7887@gmail.com>
2024-01-13 13:05:23 -0800

    add router for main and test something for app....

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {createContext , useState} from 'react';
+import Navbar from './navbar/Navbar';
+import MenuBar from './menubar/MenuBar';
+// import RegularBtn from './common/RegularBtn';
+// import Toggle from './common/Toggle';
+interface Theme{
+  isDark:boolean;
+  handleDarkMood:()=>void
+};
+export const Theme = createContext<Theme>({isDark:false , handleDarkMood:() => ''})
+
+const handleBodyStyle = (isDark:boolean):void => {
+  document.body.className = isDark ? "bg-slate-950 text-gray-100" :" text-gary-950 bg-gray-100"
+
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDark , setIsDark] = useState(true  )
+
+  const handleDarkMood = () => {
+    setIsDark(isDark => !isDark)
+  }
+  handleBodyStyle(isDark);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Theme.Provider value={{isDark , handleDarkMood}}>
+      <Navbar />
+      <MenuBar />
+
+
+    </Theme.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
