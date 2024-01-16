5f6b3413ccc9782a6e1839d77bd270a87f6860cc
mahdi gh <mahdiGhaffari7887@gmail.com>
2024-01-16 12:49:46 -0800

    three ts file for take fakedatas

---
+++ src/services/fakeCompaniesServices.ts
@@ -0,0 +1,37 @@
+const Companies = [
+    {companyId:'BDET00' , companyName:'Blizzard Entertainment' , discribtion: "Blizzard Entertainment Discribtion"},
+    {companyId:'RTGS01' , companyName:'Riot Games' , discribtion: "Riot Games Discribtion"},
+    {companyId:'ROET02' , companyName:'Rovio Entertainment' , discribtion: "Rovio Entertainment Discribtion"},
+    {companyId:'RNET03' , companyName:'Respawn Entertainment' , discribtion: "Respawn Entertainment Discribtion"},
+    {companyId:'VECN04' , companyName:'Valve Corporation' , discribtion: "Valve Corporation Discribtion"},
+    {companyId:'DICE05' , companyName:'DICE' , discribtion: "DICE Discribtion"},
+    {companyId:'RRGS06' , companyName:'Rockstar Games' , discribtion: "Rockstar Games Discribtion"},
+    {companyId:'SASO07' , companyName:'Santa Monica Studio' , discribtion: "Santa Monica Studio Discribtion"},
+    {companyId:'EAGS08' , companyName:'EA Games' , discribtion: "EA Games Discribtion"},
+    {companyId:'KIKI09' , companyName:'Konami' , discribtion: "Konami Discribtion"},
+    {companyId:'IYWD10' , companyName:'Infinity Ward' , discribtion: "Infinity Ward Discribtion"},
+    {companyId:'THTH10' , companyName:'Treyarch' , discribtion: "Treyarch Discribtion"},
+    {companyId:'SRGS10' , companyName:'Sledgehammer Games' , discribtion: "Sledgehammer Games Discribtion"},
+    {companyId:'RNSE11' , companyName:'Raven Software' , discribtion: "Raven Software Discribtion"},
+    {companyId:'LHGS12' , companyName:'Lilith Games' , discribtion: "Lilith Games Discribtion"},
+    {companyId:'GAGA13' , companyName:'Garena' , discribtion: "Garena Discribtion"},
+    {companyId:'HKSS14' , companyName:'Halfbrick Studios' , discribtion: "Halfbrick Studios Discribtion"},
+    {companyId:'PPGS15' , companyName:'PopCap Games' , discribtion: "PopCap Games Discribtion"},
+    {companyId:'IPSS16' , companyName:'Innerloop Studios' , discribtion: "Innerloop Studios Discribtion"},
+    {companyId:'MYGS17' , companyName:'Midway Games' , discribtion: "Midway Games Discribtion"},
+    {companyId:'CNGS18' , companyName:'Criterion Games' , discribtion: "Criterion Games Discribtion"},
+    {companyId:'BGET19' , companyName:'Big Moon Entertainment' , discribtion: "Big Moon Entertainment Discribtion"},
+    {companyId:'BEPS20' , companyName:'Blitwise Productions' , discribtion: "Blitwise Productions Discribtion"},
+    {companyId:'NONO21' , companyName:'Namco' , discribtion: "Namco Discribtion"},
+    {companyId:'PGSS22' , companyName:'PUBG Studios' , discribtion: "PUBG Studios Discribtion"},
+    {companyId:'CMCM23' , companyName:'Capcom' , discribtion: "Capcom Discribtion"},
+    {companyId:'FYSS24' , companyName:'Firefly Studios' , discribtion: "Firefly Studios Discribtion"},
+    {companyId:'BISS25' , companyName:'Bandai Namco Studios' , discribtion: "Bandai Namco Studios Discribtion"},
+    {companyId:'LSGS26' , companyName:'Luminous Games' , discribtion: "Luminous Games Discribtion"},
+    {companyId:'PCGS27' , companyName:'PopCap Games' , discribtion: "	PopCap Games Discribtion"},
+]
+
+
+export function getCompanies():object[]{
+    return Companies;
+}
---
+++ src/services/fakeGenreServises.ts
@@ -0,0 +1,21 @@
+const Genres = [
+    {genreId:"A00N" ,  genreName:"Action"  , genreIconImage:'../FakeData/GenreImages/Action.jpg'},
+    {genreId:"R01G" ,  genreName:"Racing" , genreIconImage:'../FakeData/GenreImages/Ricing.jpg'},
+    {genreId:"A01E" ,  genreName:"Adventure" , genreIconImage:'../FakeData/GenreImages/Adventure.jpg'},
+    {genreId:"P00E" ,  genreName:"Puzzle" , genreIconImage:'../FakeData/GenreImages/Puzzle.jpg'},
+    {genreId:"S00Y" ,  genreName:"Strategy" , genreIconImage:'../FakeData/GenreImages/Strategy.jpg'},
+    {genreId:"S01S" ,  genreName:"Sport" , genreIconImage:'../FakeData/GenreImages/Sport.jpg'},
+    {genreId:"S02G" ,  genreName:"Shooting" , genreIconImage:'../FakeData/GenreImages/Shooting.jpg'},
+    {genreId:"S03L" ,  genreName:"Survival" , genreIconImage:'../FakeData/GenreImages/Survival.jpg'},
+    {genreId:"B00D" ,  genreName:"Board" , genreIconImage:'../FakeData/GenreImages/Board.jpg'},
+    {genreId:"B01L" ,  genreName:"Battleroyal" , genreIconImage:'../FakeData/GenreImages/Battleroyal.jpg'},
+    {genreId:"M00R" ,  genreName:"Multiplayer" , genreIconImage:'../FakeData/GenreImages/Multiplayer.jpg'},
+    {genreId:"C00D" ,  genreName:"Card" , genreIconImage:'../FakeData/GenreImages/Card.jpg'},
+    {genreId:"F00G" ,  genreName:"Fighting" , genreIconImage:'../FakeData/GenreImages/Fighting.jpg'},
+    {genreId:"H00R" ,  genreName:"Horror" , genreIconImage:'../FakeData/GenreImages/Horror.jpg'},
+]
+
+
+export function getGenres() {
+    return Genres;
+}
