8a9db212c70c56076e1ab1ad354d832778dfaa2a
mahdi gh <mahdiGhaffari7887@gmail.com>
2024-01-21 03:07:04 -0800

    a utils pagniate function

--- src/utils/paginate.ts
+++ src/utils/paginate.ts
@@ -1,18 +1,39 @@
-type GamesObj ={
-        id: string;
-        name: string;
-        company: string;
-        type: string;
-        platforms: string[];
-        stock: number;
-        genres: string[];
-        image: string;
-        price: number;
-        like: boolean;
+type GamesObj = {
+  id: string;
+  name: string;
+  company: string;
+  type: string;
+  platforms: string[];
+  stock: number;
+  genres: string[];
+  image: string;
+  price: number;
+  like: boolean;
+}[];
+
+type Companies = {
+    companyImage: string;
+    companyId: string;
+    companyName: string;
+    discribtion: string;
 }[]
-export function paginate(list:GamesObj,currentPage:number , pageSize:number){
-    const startIndex = (currentPage - 1) *pageSize;
-    const finalIndex = (currentPage - 1)*pageSize + pageSize;
-    const lst = list.slice(startIndex , finalIndex);
-    return lst;
+export function paginate(
+  list: GamesObj,
+  currentPage: number,
+  pageSize: number
+) {
+  const startIndex = (currentPage - 1) * pageSize;
+  const finalIndex = (currentPage - 1) * pageSize + pageSize;
+  const lst = list.slice(startIndex, finalIndex);
+  return lst;
+}
+export function paginateCompanies(
+  list: Companies,
+  currentPage: number,
+  pageSize: number
+) {
+  const startIndex = (currentPage - 1) * pageSize;
+  const finalIndex = (currentPage - 1) * pageSize + pageSize;
+  const lst = list.slice(startIndex, finalIndex);
+  return lst;
 }
