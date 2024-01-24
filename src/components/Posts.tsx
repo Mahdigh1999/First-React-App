ef3b596a96458456051bfce1ddb42d198b4dbc78
mahdi gh <mahdiGhaffari7887@gmail.com>
2024-01-24 09:51:23 -0800

    basic post component created

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -69,9 +69,9 @@ const Posts = () => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M5 1 1 5l4 4"
             />
           </svg>
