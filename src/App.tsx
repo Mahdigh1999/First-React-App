5c8f400d859df8685c08d57ab211fc64c4ec6dc3
mahdi gh <mahdiGhaffari7887@gmail.com>
2024-01-24 09:31:21 -0800

    user context add if i forget say in last commit i say now usePage add context and use in children components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import auth from "./services/authService"
 import LoginPage from "./components/Login/LoginPage";
 import RegisterPage from "./components/Register/RegisterPage";
 import MainPage from "./components/MainPage";
+
+import "react-toastify/ReactToastify.css"
 import "./styles/App.css";
-// import Header from './Header';
-// import RegularBtn from './common/RegularBtn';
-// import Toggle from './common/Toggle';
+
 interface Theme {
   isDark: boolean;
   handleDarkMood: () => void;
@@ -24,6 +25,9 @@ const Search = createContext<Search>({
   seacrhValue: "",
   handleSearchChange: (value: React.ChangeEvent<HTMLInputElement>) => value,
 });
+
+const User = createContext({user:{}});
+
 const Page = createContext({
   currentPage: 1,
   handlePage: (value: number) => {},
@@ -43,11 +47,23 @@ export const useTheme = () => {
 export const usePage = () => {
   return useContext(Page);
 };
+export const useUser = () => {
+  return useContext(User);
+};
 
 function App() {
   const [isDark, setIsDark] = useState(true);
   const [seacrhValue, setSearchValue] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const user = auth.getCurrentUser();
+    setUser(user);
+
+
+    
+  },[])
+
   const handleDarkMood = () => {
     setIsDark((isDark) => !isDark);
   };
@@ -58,29 +74,32 @@ function App() {
   const handlePage = (page: number) => {
     setCurrentPage(page);
   };
+
   handleBodyStyle(isDark);
 
   return (
     <>
-      <Search.Provider value={{ seacrhValue, handleSearchChange }}>
-        <Theme.Provider value={{ isDark, handleDarkMood }}>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route
-              path="/*"
-              element={
-                <Page.Provider value={{ currentPage, handlePage }}>
-                  <MainPage />
-                </Page.Provider>
-              }
-            ></Route>
-          </Routes>
+      <User.Provider value={{user}}>
+        <Search.Provider value={{ seacrhValue, handleSearchChange }}>
+          <Theme.Provider value={{ isDark, handleDarkMood }}>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route
+                path="/*"
+                element={
+                  <Page.Provider value={{ currentPage, handlePage }}>
+                    <MainPage />
+                  </Page.Provider>
+                }
+              ></Route>
+            </Routes>
 
-          {/* <LoginPage /> */}
-          {/* <RegisterPage/> */}
-        </Theme.Provider>
-      </Search.Provider>
+            {/* <LoginPage /> */}
+            {/* <RegisterPage/> */}
+          </Theme.Provider>
+        </Search.Provider>
+      </User.Provider>
     </>
   );
 }
