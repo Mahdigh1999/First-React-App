acf748250d209cd5068724ea844e599b9b259295
mahdi gh <mahdiGhaffari7887@gmail.com>
2024-01-24 09:55:50 -0800

    check status and use that boolean props passed to chiled

--- src/components/common/inputs/CheckBoxInput.tsx
+++ src/components/common/inputs/CheckBoxInput.tsx
@@ -1,19 +1,26 @@
+import React from 'react'
+
 type Props = {
     className?:string
+    checked?:boolean;
+    setChecked?:() => void;
     label:string
 }
-function CheckBoxInput({label}:Props) {
+function CheckBoxInput({label ,className, checked , setChecked}:Props) {
+
     return (
-        <div className="flex items-start mb-5">
-        <div className="flex items-center h-5">
+        <div className={className? className : " flex  items-center mb-5 "}>
+
           <input
+            onInput={setChecked}
+            checked = {checked}
             id="remember"
             type="checkbox"
             value=""
             className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 "
 
           />
-        </div>
+
         <label htmlFor="remember" className="select-none ms-2 text-sm font-medium ">
           {label}
         </label>
