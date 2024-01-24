4ddee27b775d1c72e57aa30f25d68d7c3946eda7
mahdi gh <mahdiGhaffari7887@gmail.com>
2024-01-24 09:54:01 -0800

    disable status and className needed and now handled

--- src/components/common/RegularBtn.tsx
+++ src/components/common/RegularBtn.tsx
@@ -1,15 +1,22 @@
 type BtnProps = {
-    type?: "submit" | "reset" | "button" | undefined
-    className?:string
-    children:string
-}
+  type?: "submit" | "reset" | "button" | undefined;
+  className?: string;
+  disable?: boolean;
+  children: string;
+};
 
-function RegularBtn({type , className , children}:BtnProps) {
+function RegularBtn({ type, className, children, disable }: BtnProps) {
   return (
     <>
       <button
-        type ={type ? type : "submit"}
-        className={className ? className :  "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center "}
+        disabled={disable ? disable : false}
+        type={type ? type : "submit"}
+        className={
+          (disable ? " cursor-not-allowed " : "") +
+          (className
+            ? className
+            : "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center ")
+        }
       >
         {children}
       </button>
