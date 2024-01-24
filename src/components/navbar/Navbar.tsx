32e1c01b94bd228ff0fd99a3d6f01ffc14cc705c
mahdi gh <mahdiGhaffari7887@gmail.com>
2024-01-24 10:04:48 -0800

    add classname for Logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import SearchBox from "../common/SearchBox";
 import Logo from "../common/Logo";
 function Navbar() {
   return (
-    <nav className=" w-full h-auto z-20 py-2  border-b border-gray-400 flex justify-around items-center space-x-3 px-3">
+    <nav id="navbar" className=" w-full h-auto z-20 py-2  border-b border-gray-400 flex justify-around items-center space-x-3 px-3">
       <Logo/>
       <SearchBox />
       <Toggle />
